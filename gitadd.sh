6621aab6f39ef313c6b1520f60cd06c77ab5cac8
AlekseyMohylnyi <alloexey@gmail.com>
2022-02-04 20:04:42 +0200

    new commit

--- gitadd.sh
+++ gitadd.sh
@@ -5,6 +5,8 @@ set -e
 git init
 git add -A
 git commit -m 'new commit'
+git config --global core.safecrlf warn
+git config --global core.autocrlf true
 git remote add origin https://github.com/AlekseyMohylnyi/moonriver.git
 git branch -M main
 git push -u origin main
